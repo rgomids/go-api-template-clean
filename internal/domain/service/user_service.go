fd4fa9aa39d1706e9bb189f03b844812414f85af
GOMIDES, Rafael <mrrafagomides@gmail.com>
2025-07-16 21:49:24 -0300

    Add base domain modules with User entity

---
+++ internal/domain/service/user_service.go
@@ -0,0 +1,9 @@
+package service
+
+import "github.com/seuusuario/go-api-template-clean/internal/domain/entity"
+
+// UserService exposes user-related business operations.
+type UserService interface {
+	RegisterUser(name, email string) (*entity.User, error)
+	RemoveUser(id string) error
+}
